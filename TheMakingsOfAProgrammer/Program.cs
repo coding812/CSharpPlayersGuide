ebe1f9692df95944a06a752fa761057df5406047
unknown <coding812@gmail.com>
2024-03-14 23:55:55 -0400

    First Commit

---
+++ TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I'm Going to finish Learning C#!");
+Console.WriteLine("I will be a great programmer!");
+Console.WriteLine("I will Master C#, and many other languages and tools!");
+Console.WriteLine("I can't wait to finish it all and build something amazing!");
