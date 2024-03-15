de16c62bc85fe83691d1f27efbdd5e47028ddfbd
unknown <coding812@gmail.com>
2024-03-15 01:06:18 -0400

    worked through to variables

---
+++ ConsolasAndTelim/Program.cs
@@ -0,0 +1,6 @@
+﻿string name;
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is this bread for?");
+name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got bread.");
+
---
+++ ThingNamer3000/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Re-wrote program with descriptive variable names 
+    Also commented what each line does*/
+string ofDoom = "of Doom";
+string modelNumber = "3000";
+// Prompt user for input and assign to variable
+Console.WriteLine("What kind of thing are we talking about?");
+string thing = Console.ReadLine();
+// Prompt user for input and assign to variable
+Console.WriteLine("how would you describe it? Big? Azure? Tattered?");
+string description = Console.ReadLine();
+// Print out the result of all variables to the console
+Console.WriteLine("The " + description + " " + thing + " " + ofDoom + " " + modelNumber + "!");
