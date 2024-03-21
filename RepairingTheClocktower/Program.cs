6bf6bc0e00b5cc3206140cd8f8827e6483725b64
unknown <coding812@gmail.com>
2024-03-20 22:23:41 -0400

    Added the repairing the clocktower challenge, with expanded solution, as well as one-liner solution

---
+++ RepairingTheClocktower/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// Original version
+
+// Console.WriteLine("Enter a number and I'll tell you if it should make the clock tick, or tock: ");
+// bool isEven = Convert.ToInt32(Console.ReadLine()) % 2 == 0;
+
+// if (isEven)
+// {
+//     Console.WriteLine("tick");
+// }
+// else
+// {
+//     Console.WriteLine("tock");
+// }
+
+// Shortened version
+Console.WriteLine("Enter a number and I'll tell you if it should make the clock tick, or tock: ");
+Console.WriteLine(Convert.ToInt32(Console.ReadLine()) % 2 == 0 ? "tick" : "tock");
