28630d88e2b7045282da29675c2e1de5b613cb77
unknown <coding812@gmail.com>
2024-03-22 11:11:18 -0400

    Completed level 11 challenges

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,35 @@
+﻿for (int blast = 1; blast <= 100; blast++)
+{
+    if (blast % 3 == 0 && blast % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{blast}: Electric and Fire");
+        Console.ResetColor();
+    }
+    else if (blast % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{blast}: Fire");
+        Console.ResetColor();
+    }
+    else if (blast % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{blast}: Electric");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.WriteLine($"{blast}: Normal");
+    }
+}
+
+
+// for (int blast = 1; blast <= 100; blast++)
+// {
+//     string blastType;
+//     blastType = (blast % 3 == 0 && blast % 5 == 0) ? "Electric and Fire" :
+//                 (blast % 3 == 0) ? "Fire" :
+//                 (blast % 5 == 0) ? "Electric" : "Normal";
+//     Console.WriteLine($"{blast}: {blastType}");
+// }
---
+++ ThePrototype/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Title = "The Prototype";
+
+// Welcome message, Set Font color to Red for Player 1
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("User 1, enter a number between 0 and 100: ");
+int userOnePosition = Convert.ToInt16(Console.ReadLine()); // Assign user input to userOnePosition
+
+// Validate user input
+while (userOnePosition < 0 || userOnePosition > 100)
+{
+    Console.WriteLine("Invalid number, please enter a number between 0 and 100");
+    userOnePosition = Convert.ToInt16(Console.ReadLine());
+}
+
+// Clear the console and reset the font color
+Console.ResetColor();
+Console.Clear();
+
+// Set Font color to Blue for Player 2
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("User 2, Guess the number.");
+Console.Write("What is your guess? ");
+int userTwoGuess = Convert.ToInt16(Console.ReadLine()); // Assign user input to userTwoGuess
+
+// Validate user input And check if the userTwoGuess is equal to userOnePosition
+while (userTwoGuess != userOnePosition)
+{
+    if (userTwoGuess < userOnePosition)
+    {
+        Console.WriteLine($"{userTwoGuess} is too low.");
+    }
+    else
+    {
+        Console.WriteLine($"{userTwoGuess} is too high.");
+    }
+
+    Console.Write("What is your guess? ");
+    userTwoGuess = Convert.ToInt16(Console.ReadLine());
+}
+
+// Return to user when they make correct guess and reset the font color
+Console.WriteLine("Congratulations! You guessed the number!");
+Console.ResetColor();
